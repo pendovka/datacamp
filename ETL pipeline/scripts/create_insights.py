1068ee179381060b81f109d9ad0fe30460992209
alexei <137596628+pendovka@users.noreply.github.com>
2023-12-17 01:21:37 -0500

    Update create_insights.py

--- ETL pipeline/scripts/create_insights.py
+++ ETL pipeline/scripts/create_insights.py
@@ -1,18 +1,5 @@
 from common.base import session
 
-# Create the view with the appropriate metrics
-query = """
-CREATE OR REPLACE VIEW insights AS
-SELECT county,
-       COUNT(*) AS sales_count,
-       SUM(CAST(price AS int)) AS sales_total,
-       MAX(CAST(price AS int)) AS sales_max,
-       MIN(CAST(price AS int)) AS sales_min,
-       AVG(CAST(price AS int))::numeric(10,2) AS sales_avg
-FROM ppr_clean_all
-GROUP BY county
-"""
-
 def main():
        session.execute(query)
        session.commit()
