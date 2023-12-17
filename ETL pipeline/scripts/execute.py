3f6a9f4b2e4500fe8ec4326441e1b891006454c7
alexei <137596628+pendovka@users.noreply.github.com>
2023-12-17 01:19:48 -0500

    Update execute.py

--- ETL pipeline/scripts/execute.py
+++ ETL pipeline/scripts/execute.py
@@ -1,8 +1,12 @@
 import extract
 import transform
 import load
+import create_insights
+import insights_export
 
 if __name__ == "__main__":
     extract.main()
     transform.main()
     load.main()
+    create_insights.main()
+    insights_export.main()
